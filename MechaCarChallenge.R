582536be7bccea8d4e394f9f9a48c4aab87afc8d
Brittany Marchand <94507822+blmarcha7@users.noreply.github.com>
2022-04-17 21:35:35 -0400

    Updating R file and adding images

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -11,6 +11,12 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 #Generate summary 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_table))
 
-
-
-
+#Deliverable 2
+#read in the suspension coil file
+Suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+#use summarize to get total_summary table
+total_summary <- summarize(Suspension_Coil,Mean=mean(PSI),Median=median(PSI),Variance=var(PSI), SD=sd(PSI)) #create summary table
+View(total_summary)
+#lot summary using groupby and summarize
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') 
+View(lot_summary)
