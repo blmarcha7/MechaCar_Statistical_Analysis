1b5c804a35de48984d1f042a4332a913690cf056
Brittany Marchand <94507822+blmarcha7@users.noreply.github.com>
2022-04-19 21:11:58 -0400

    Adding updated file with deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -20,3 +20,14 @@ View(total_summary)
 #lot summary using groupby and summarize
 lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') 
 View(lot_summary)
+
+#Deliverable3
+#One sample t-test
+t.test(Suspension_Coil$PSI,mu=1500)
+
+#multiple tests using subset
+t.test(subset(Suspension_Coil, Manufacturing_Lot == "Lot1")$PSI, mu=1500)
+
+t.test(subset(Suspension_Coil, Manufacturing_Lot == "Lot2")$PSI, mu=1500)
+
+t.test(subset(Suspension_Coil, Manufacturing_Lot == "Lot3")$PSI, mu=1500)
